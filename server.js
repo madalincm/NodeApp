76e50a37dda902f4484148d2007db43db576ea89
madalincm <madalin.cotetiu@softvision.ro>
2018-05-02 19:06:49 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) =>{
+    //res.send("<h1>Hello Express</h1>");
+    res.send({
+        name: "Madalin",
+        likes: [
+            "biking",
+            "cities"
+        ]
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+
+app.get("/bad", (req, res) =>{
+    res.send({
+        errorMessage: "bad request"
+    });
+});
+
+app.listen(3000);
